e2c2b10d56c1f04b1f00e819549a5045b197232a
Varani07 <matheusvarani@gmail.com>
2024-10-01 10:40:58 -0300

    Adicionando projeto

---
+++ calculo.php
@@ -0,0 +1,172 @@
+<?php
+    if(isset($_POST['btn1'])){
+        $lado = $_POST['numAreaQuadrado'];
+        
+        if(is_numeric($lado)){
+            $area = $lado ** 2;
+            echo "<h2>A área do quadrado é: $area</h2>";
+        }else{
+            echo '<h2>Digite um número!</h2>';
+        }
+        echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+    }else{
+        if(isset($_POST['btn2'])){
+            $valor_A = $_POST['numValorA'];
+            $valor_B = $_POST['numValorB'];
+            
+            if(is_numeric($valor_A) && is_numeric($valor_B)){
+                $valor_C = ($valor_A + $valor_B)/2;
+                $valor_D = (($valor_A + $valor_B)*2)/2;
+                echo "<h2>Valor de A + Valor de B divido por 2: $valor_C</h2>";
+                echo "<h2>O dobro do Valor de A + Valor de B divido por 2: $valor_D</h2>";
+            }else{
+                echo '<h2>Digite um número!</h2>';
+            }
+            echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+        }else{
+            if(isset($_POST['btn3'])){
+                $valor_A = $_POST['numValorA2'];
+                $valor_B = $_POST['numValorB2'];
+
+                if(is_numeric($valor_A) && is_numeric($valor_B)){
+                    $resultado = ($valor_A - $valor_B)**2;
+                    echo "<h2>O quadrado da diferença entre os dois números é: $resultado</h2>";
+                }else{
+                    echo '<h2>Digite um número!</h2>';
+                }
+                echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+            }else{
+                if(isset($_POST['btn4'])){
+                    $valor_A = $_POST['numValorA3'];
+                    $valor_B = $_POST['numValorB3'];
+                    $valor_C = $_POST['numValorC3'];
+
+                    if(is_numeric($valor_A) && is_numeric($valor_B) && is_numeric($valor_C)){
+                        $resultado = ($valor_A**2) + ($valor_B**2) + ($valor_C**2);
+                        echo "<h2>A soma do quadrado dos números é: $resultado</h2>";
+                    }else{
+                        echo '<h2>Digite um número!</h2>';
+                    }
+                    echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+                    
+                }else{
+                    if(isset($_POST['btn5'])){
+                        $valor_A = $_POST['numValorA4'];
+                        $valor_B = $_POST['numValorB4'];
+                        $valor_C = $_POST['numValorC4'];
+
+                        if(is_numeric($valor_A) && is_numeric($valor_B) && is_numeric($valor_C)){
+                            $resultado = ($valor_A + $valor_B + $valor_C) ** 2;
+                            echo "<h2>O quadrado da soma dos números é: $resultado</h2>";
+                        }else{
+                            echo '<h2>Digite um número!</h2>';
+                        }
+                        echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+                        
+                    }else{
+                        if(isset($_POST['btn6'])){
+                            $kilos = $_POST['numKg'];
+
+                            if(is_numeric($kilos)){
+                                $emagreceu = $kilos - (($kilos * 20)/100);
+                                $engordou = $kilos + (($kilos * 15)/100);
+                                
+                                echo "<h2>Peso se engordar mais 15%: $engordou Kg</h2>";
+                                echo "<h2>Peso se emagrecer 20%: $emagreceu Kg</h2>";
+                            }else{
+                                echo '<h2>Digite um número!</h2>';
+                            }
+                            echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+                        }else{
+                            if(isset($_POST['btn7'])){
+                                $valor_A = $_POST['numValorA5'];
+                                $valor_B = $_POST['numValorB5'];
+                                $valor_C = $_POST['numValorC5'];
+
+                                if(is_numeric($valor_A) && is_numeric($valor_B) && is_numeric($valor_C)){
+                                    echo "<h2>Resultado:</h2><br/>";    
+                                    $resultado_positivo = (-1 * $valor_B + sqrt(($valor_B ** 2 - 4 * $valor_A * $valor_C))) / 2 * $valor_A;
+                                    $resultado_negativo = (-1 * $valor_B - sqrt(($valor_B ** 2 - 4 * $valor_A * $valor_C))) / 2 * $valor_A;
+                                    echo "<h2>x = $resultado_positivo </h2><br/>";
+                                    echo "<h2>x = $resultado_negativo </h2><br/>";
+                                }else{
+                                    echo '<h2>Digite um número!</h2>';
+                                }
+                                echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+                                
+                            }else{
+                                if(isset($_POST['btn8'])){
+                                    $valor_por_litro = $_POST['numLitro'];
+                                    $kilometro_por_litro = $_POST['numKmL'];
+                                    $distancia = $_POST['numDistancia'];
+
+                                    if(is_numeric($valor_por_litro) && is_numeric($kilometro_por_litro) && is_numeric($distancia)){
+                                        $litros_necessarios = $distancia / $kilometro_por_litro;
+                                        $quantia_gasta = $valor_por_litro * $litros_necessarios;
+                                        
+                                        echo "<h2>Serão necessários $litros_necessarios Litros para percorrer esta distância.</h2><br/>";
+                                        echo "<h2>Será gasto R$$quantia_gasta.</h2><br/>";
+                                    }else{
+                                        echo '<h2>Digite um número!</h2>';
+                                    }
+                                    echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+
+                                }else{
+                                    if(isset($_POST['btn9'])){
+                                        $primeiro_angulo = $_POST['num1Ang'];
+                                        $segundo_angulo = $_POST['num2Ang'];
+
+                                        if(is_numeric($primeiro_angulo) && is_numeric($segundo_angulo)){
+                                            $terceiro_angulo = 180 - ($primeiro_angulo + $segundo_angulo);
+
+                                            echo "<h2>Medida do terceiro ângulo: $terceiro_angulo</h2><br/>";
+                                        }else{
+                                            echo '<h2>Digite um número!</h2>';
+                                        }
+                                        echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+                                    
+                                    }else{
+                                        if(isset($_POST['btn10'])){
+                                            $publico_total = $_POST['numPubTotal'];
+
+                                            if(is_numeric($publico_total)){
+                                                $resultado = ((($publico_total*10)/100)*1) + ((($publico_total*50)/100)*5) + ((($publico_total*30)/100)*10) + ((($publico_total*10)/100)*20);
+
+                                                echo "<h2>A renda do jogo foi de: R$$resultado</h2><br/>";
+                                            }else{
+                                                echo '<h2>Digite um número!</h2>';
+                                            }
+                                            echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+
+                                        }else{
+                                            if(isset($_POST['btn11'])){
+                                                $valor_x1 = $_POST['numx1'];
+                                                $valor_y1 = $_POST['numy1'];
+                                                $valor_x2 = $_POST['numx2'];
+                                                $valor_y2 = $_POST['numy2'];
+
+                                                if(is_numeric($valor_x1) && is_numeric($valor_y1) && is_numeric($valor_x2) && is_numeric($valor_y2)){
+                                                    $resultado = sqrt(($valor_x2 - $valor_x1)**2 + ($valor_y2 - $valor_y1)**2);
+
+                                                    echo "<h2>Distância entre os dois pontos: $resultado</h2><br/>";
+                                                }else{
+                                                    echo '<h2>Digite um número!</h2>';
+                                                }
+                                                echo '<br/><a href="index.php" target="self">Clique aqui para voltar</a>';
+
+                                            }else{
+                                                echo 'Use o form para envio!<br/>';
+                                                echo '<a href="index.php" target="self">Clique aqui para voltar</a>';
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+?>
+
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercícios</title>
+    </head>
+    <body>
+        <form name="frmExercicios" action="calculo.php" method="post">
+            <h2>1. Digite o tamanho do lado de um quadrado para receber a área: </h2><br/>
+            <input type="number" name="numAreaQuadrado" value=""/>
+            <input type="submit" name="btn1" value="Enviar"/>
+            
+            <h2>2. Dê um valor para A e B respectivamente: </h2><br/>
+            <h2>A = </h2><input type="number" name="numValorA" value=""/>
+            <h2>B = </h2><input type="number" name="numValorB" value=""/>
+            <input type="submit" name="btn2" value="Enviar"/>
+            
+            <h2>3. Dê um valor para A e B respectivamente para calcular o quadrado da diferença dos números: </h2><br/>
+            <h2>A = </h2><input type="number" name="numValorA2" value=""/>
+            <h2>B = </h2><input type="number" name="numValorB2" value=""/>
+            <input type="submit" name="btn3" value="Enviar"/>
+            
+            <h2>4. Dê um valor para A, B e C respectivamente para calcular a soma do quadrado dos números: </h2><br/>
+            <h2>A = </h2><input type="number" name="numValorA3" value=""/>
+            <h2>B = </h2><input type="number" name="numValorB3" value=""/>
+            <h2>C = </h2><input type="number" name="numValorC3" value=""/>
+            <input type="submit" name="btn4" value="Enviar"/>
+            
+            <h2>5. Dê um valor para A, B e C respectivamente para calcular o quadrado da soma dos números: </h2><br/>
+            <h2>A = </h2><input type="number" name="numValorA4" value=""/>
+            <h2>B = </h2><input type="number" name="numValorB4" value=""/>
+            <h2>C = </h2><input type="number" name="numValorC4" value=""/>
+            <input type="submit" name="btn5" value="Enviar"/>
+            
+            <h2>6. Digite o peso de uma pessoa (kg): </h2><br/>
+            <input type="number" name="numKg" value=""/>
+            <input type="submit" name="btn6" value="Enviar"/>
+            
+            <h2>7. Dê um valor para A, B e C respectivamente para calcular a a Báscara e retornar o valor de X: </h2><br/>
+            <h2>A = </h2><input type="number" name="numValorA5" value=""/>
+            <h2>B = </h2><input type="number" name="numValorB5" value=""/>
+            <h2>C = </h2><input type="number" name="numValorC5" value=""/>
+            <input type="submit" name="btn7" value="Enviar"/>
+            
+            <h2>8. Dê um valor para saber quantos litros serão necessários para percorrer a distância escolhida e quanto será gasto: </h2><br/>
+            <h2>Preço por litro (R$): </h2><input type="number" name="numLitro" value=""/>
+            <h2>Km/L do carro: </h2><input type="number" name="numKmL" value=""/>
+            <h2>Distância a percorrer (km): </h2><input type="number" name="numDistancia" value=""/>
+            <input type="submit" name="btn8" value="Enviar"/>
+            
+            <h2>9. Informe a medida de dois ângulos de um triangulo e receba a medida do terceiro ângulo: </h2><br/>
+            <h2>Primeiro Ângulo: </h2><input type="number" name="num1Ang" value=""/>
+            <h2>Segundo Ângulo: </h2><input type="number" name="num2Ang" value=""/>
+            <input type="submit" name="btn9" value="Enviar"/>
+            
+            <h2>10. Informe o público total de um jogo de futebol para calcular a renda do jogo: </h2><br/>
+            <h2>Público Total: </h2><input type="number" name="numPubTotal" value=""/>
+            <input type="submit" name="btn10" value="Enviar"/>
+            
+            <h2>11. Informe quatro valores para calcular a distância entre dois pontos: </h2><br/>
+            <h2>x1: </h2><input type="number" name="numx1" value=""/>
+            <h2>y1: </h2><input type="number" name="numy1" value=""/>
+            <h2>x2: </h2><input type="number" name="numx2" value=""/>
+            <h2>y2: </h2><input type="number" name="numy2" value=""/>
+            <input type="submit" name="btn11" value="Enviar"/>
+        </form>
+    </body>
+</html>
